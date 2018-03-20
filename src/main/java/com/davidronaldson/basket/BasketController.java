2d23040355ffaf0b087ceb53dbcf67060c89a9a1
davidronaldson <davay86@gmail.com>
2018-03-20 13:58:54 +0000

    Adding extra methods

--- src/main/java/com/davidronaldson/basket/BasketController.java
+++ src/main/java/com/davidronaldson/basket/BasketController.java
@@ -1,6 +1,7 @@
 package com.davidronaldson.basket;
 
 import com.davidronaldson.requestBean.Item;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,10 @@ import java.util.List;
 public class BasketController {
 
     List<Item> availableItems;
+    List<Item> items;
 
-    @RequestMapping(value="/items", method = RequestMethod.GET)
-    public List<Item> greeting(){
+    @RequestMapping(value="/availableItems", method = RequestMethod.GET)
+    public List<Item> getAvailableItems(){
         availableItems = new ArrayList<>();
         availableItems.add(new Item("Apple",new BigDecimal(.99)));
         availableItems.add(new Item("Bread", new BigDecimal(1.49)));
@@ -23,4 +25,26 @@ public class BasketController {
         availableItems.add(new Item("Soup", new BigDecimal(1.25)));
         return availableItems;
     }
+
+    @RequestMapping(value="/items", method = RequestMethod.GET)
+    public List<Item> getBasketItems(){
+        items = new ArrayList<>();
+        return items;
+    }
+
+    @PostMapping(value="/addItems")
+    public void addItem(List<Item> items){
+        for (Item item : items) {
+            this.items.add(item);
+        }
+    }
+
+    @PostMapping(value="/removeItems")
+    public void removeItem(List<Item> items){
+        for (Item item : items) {
+            this.items.remove(item);
+        }
+    }
+
+
 }
